1a4a6979c5180df3f1a9b737e2547cafba0bbb0d
Farhan899 <farhanbhai2u@gmail.com>
2026-01-09 02:44:32 +0500

    6t

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import asyncio
 from app.main import app
+
+
+vercel_app = app
 
 # Make sure the FastAPI app instance is available as 'app' for Vercel
 # This satisfies Vercel's requirement to find the FastAPI application
